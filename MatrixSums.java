dce9b1364f667e5a5339e00aabaf8a505048e4c5
afhambaigbaig-png <afhambaigbaig@gmail.com>
2026-02-27 09:47:48 -0800

    Add files via upload

---
+++ MatrixSums.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+ class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input rows and columns
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        // Input matrix elements
+        System.out.println("Enter matrix elements:");
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Sum of rows
+        System.out.println("\nSum of each row:");
+        for(int i = 0; i < rows; i++) {
+            int rowSum = 0;
+            for(int j = 0; j < cols; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Row " + i + " sum = " + rowSum);
+        }
+
+        // Sum of columns
+        System.out.println("\nSum of each column:");
+        for(int j = 0; j < cols; j++) {
+            int colSum = 0;
+            for(int i = 0; i < rows; i++) {
+                colSum += matrix[i][j];
+            }
+            System.out.println("Column " + j + " sum = " + colSum);
+        }
+
+        // Diagonal sums (only for square matrix)
+        if(rows == cols) {
+            int mainDiagonalSum = 0;
+            int reverseDiagonalSum = 0;
+
+            for(int i = 0; i < rows; i++) {
+                mainDiagonalSum += matrix[i][i];
+                reverseDiagonalSum += matrix[i][rows - 1 - i];
+            }
+
+            System.out.println("\nMain Diagonal Sum = " + mainDiagonalSum);
+            System.out.println("Reverse Diagonal Sum = " + reverseDiagonalSum);
+        } else {
+            System.out.println("\nDiagonal sums not possible (Matrix is not square).");
+        }
+
+        sc.close();
+    }
+}
+
+
